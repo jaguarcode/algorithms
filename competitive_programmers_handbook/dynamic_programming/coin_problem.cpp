5f149fd2578f0fe9f2c02d9d5280348671b8bb1a
IKHYEON, KIM <behonz@gmail.com>
2020-11-13 11:21:44 +0900

    commit from Z8

--- competitive_programmers_handbook/dynamic_programming/coin_problem.cpp
+++ competitive_programmers_handbook/dynamic_programming/coin_problem.cpp
@@ -4,11 +4,65 @@
 **/
 #include <bits/stdc++.h>
 using namespace std;
+
+/**
+ * x < 0, 	infinity
+ * x = 0, 	0
+ * x > 0,	min(solve(x-c)+1), c == coins
+ */
+const int MAXN = 1e9+5;
+
+vector<int> coins = {1,3,4};
+int cnt = 0;
+
+int solve(int x) {
+	if(x == 0) return 0;
+	if(x < 0) return MAXN;
+	int best = MAXN;
+	for(auto c : coins)
+		best = std::min(best, solve(x-c)+1);
+	return best;
+}
+
+vector<bool> ready;
+vector<int> value;
+
+int solve_with_memoization(int x) {
+	if(x == 0) return 0;
+	if(x < 0) return MAXN;
+	if(ready[x]) return value[x];
+	int best = MAXN;
+	for(auto c : coins)
+		best = std::min(solve_with_memoization(x-c)+1, best);
+	ready[x] = true;
+	value[x] = best;
+	return best;
+}
+
+int solve_with_loop(int n) {
+	value[0] = 0;
+	for(int x = 1; x <= n; x++) {
+		value[x] = MAXN;
+		for(auto c : coins) {
+			if(x-c >= 0) value[x] = std::min(value[x], value[x-c]+1); 
+		}
+	}
+	return value[n];
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	
+	int n;
+	cin >> n;
+	ready.resize(n);
+	value.resize(n);
 	
+	//int answer = solve(n);
+	//int answer = solve_with_memoization(n);
+	int answer = solve_with_loop(n);
+	cout << answer << "\n";
 	
 	return 0;
 }
--- cses/1624/a.cpp
+++ cses/1624/a.cpp
@@ -4,20 +4,26 @@
 **/
 #include <bits/stdc++.h>
 using namespace std;
-vector<bool> board, col, diag1, diag2;
-const int n = 8;
+
+vector<char> directions = {'D','U','L','R'};
+vector<char> grid;
+int n;
 int cnt = 0;
-void search(int y) {
-	if(y == n) {
+void search(int k) {
+	if(k == n) {
 		cnt++;
 		return;
 	} else {
 		for(int x = 0; x < n; x++) {
-			if(board[x+(y*n)]) continue;
-			if(col[x] || diag1[x+y] || diag2[x-y+n-1]) continue;
-			col[x] = diag1[x+y] = diag2[x-y+n-1] = true;
-			search(y+1);
-			col[x] = diag1[x+y] = diag2[x-y+n-1] = false;
+			for(int y = 0; y < n; y++) {
+				for(int i = 0; i < (int)directions.size(); i++) {
+					if(grid[x+(y*n)] == 'D' || grid[x+(y*n)] == 'U' 
+						|| grid[x+(y*n)] == 'L' || grid[x+(y*n)] == 'R') continue;
+					grid[x+(y*n)] = directions[i];
+					search(k+1);
+					grid[x+(y*n)] = '?';
+				}
+			}
 		}
 	}
 }
@@ -25,21 +31,16 @@ void search(int y) {
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	board.resize(n*n);
-	col.resize(n);
-	diag1.resize(n+n);
-	diag2.resize(n+n);
-	
-	for(int i = 0; i < n*n; i++) {
-		char ch; cin >> ch;
-		if(ch == '.') board[i] = false;
-		else if(ch == '*') board[i] = true;
-	}
+
+	cin >> n;
+	grid.resize(n*n);
 	
 	search(0);
 	
 	cout << cnt << "\n";
 	
+	for(auto c : grid) cout << c << " ";
+	cout << "\n";
+		
 	return 0;
 }
