76b428df38517dfa875658f8be7f7d2c1a683123
IKHYEON.KIM <behonz@gmail.com>
2021-03-04 17:24:42 -0800

    commit from Z8

---
+++ cses/2205/b.cpp
@@ -0,0 +1,31 @@
+/**
+ * author: jaguarcode
+ * date:
+**/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	/*
+	 * A Gray code is a list of all 2n bit strings of length n, 
+	 * where any two successive strings differ in exactly one bit 
+	 * (i.e., their Hamming distance is one).
+	 * Your task is to create a Gray code for a given length n.
+	 * Example
+
+		Input:
+		2
+
+		Output:
+		00
+		01
+		11
+		10
+	 */
+	
+	
+	
+	return 0;
+}
