36ff31549b85892e97ebf40d15da3d371d41b7da
IKHYEON.KIM <behonz@gmail.com>
2021-03-14 19:32:28 -0700

    commit from Z8

--- cses/2205/b.cpp
+++ cses/2205/b.cpp
@@ -7,15 +7,25 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+
+	int n;
+	cin >> n;
 	
-	/*
-	 * A Gray code is a list of all 2n bit strings of length n, 
-	 * where any two successive strings differ in exactly one bit 
-	 * (i.e., their Hamming distance is one).
-	 * Your task is to create a Gray code for a given length n.
-	 */
+	int cnt = pow(2, n);
+	vector<string> p = { "0", "1", "1", "0" };
 	
+	vector<string> v(cnt);
+	int r = pow(2, n-1);
 	
+	for(int i = n; i > 0; i--) {
+		for(size_t j = 0; j < v.size(); j++) {
+			string& c = v[j];
+			c += p[(j/r)%p.size()];
+		}
+		r /= 2;
+	}
+	
+	for(auto a : v) cout << a << "\n";
 	
 	return 0;
 }
