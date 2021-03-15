f4ab5b17516fb5f8fc72641120abc2980699eb4d
IKHYEON.KIM <behonz@gmail.com>
2021-03-14 19:33:26 -0700

    commit from Z8

---
+++ competitive_programmers_handbook/temp/temp.cpp
@@ -0,0 +1,22 @@
+/**
+ * author: jaguarcode
+ * date:
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	long long s = 1;
+	const int m = 1000000007;
+	while(n--) s = (s * 2) % m;
+	cout << s << "\n";
+	
+	return 0;
+}
